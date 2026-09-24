8bc4b491b0d07de67723e2fc8979bd780b041b72
Soham_Shrawankar@ <soham.shrawankar.btech2022@sitnagpur.siu.edu.in>
2026-09-24 11:08:38 +0530

    Add dictionary manipulation examples in python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+""" dictionaries practise """ 
+
+my_dict = {"name":"soham" , "city":"nagpur" , "phone":"937147763"}
+my_dict["phone"] = 9307147763
+my_dict["name"] = "purva"
+my_dict["occupation"] = "student"
+my_dict.pop("city")
+print(my_dict)
+
+keys = my_dict.keys()
+print(keys)
